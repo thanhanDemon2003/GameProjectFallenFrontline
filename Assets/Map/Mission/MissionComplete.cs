b8b935b3cd5046a00d1bc714f50c541ef0b69dc3
ZerasKasper <130994711+ZerasKasper@users.noreply.github.com>
2023-11-19 13:29:48 +0700

    commit test mission

---
+++ Assets/Map/Mission/MissionComplete.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MissionComplete : MonoBehaviour
+{
+    [Header("Objectives to Complete")]
+    public TextMeshPro Mission_1;
+    public TextMeshPro Mission_2;
+    public TextMeshPro Mission_3;
+
+    public static MissionComplete occurrence;
+
+    private void Awake()
+    {
+        occurrence = this;
+    }
+
+    public void GetObjectiveDone(bool obj1, bool obj2, bool obj3)
+    {
+        if(obj1 == true)
+        {
+            Mission_1.text = "Collected full Document!";
+            Mission_1.color = Color.green;
+        }
+        else
+        {
+            Mission_1.text = "Find and collect 5 Document!";
+            Mission_1.color = Color.green;
+        }
+        if (obj2 == true)
+        {
+            Mission_2.text = "Collected Virus Sample!";
+            Mission_2.color = Color.green;
+        }
+        else {
+            Mission_2.text = "Find and collect Virus Sample!";
+            Mission_2.color = Color.green;
+        }
+        if (obj3 == true)
+        {
+            Mission_3.text = "Get out Lab!";
+            Mission_3.color = Color.green;
+        }
+        else
+        {
+            Mission_3.text = "Get out Lab!";
+            Mission_3.color = Color.green;
+        }
+    }
+}
---
+++ Assets/Map/Mission/RandomEnemies.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEnemies : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int initialEnemyCount = 5;
+    public int maxEnemies = 8; 
+    public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        InitializeEnemies();
+    }
+
+    void InitializeEnemies()
+    {
+        for (int i = 0; i < initialEnemyCount; i++)
+        {
+            Transform spawnPoint = GetRandomSpawnPoint();
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length < maxEnemies)
+        {
+            Transform spawnPoint = GetRandomSpawnPoint();
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    Transform GetRandomSpawnPoint()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)];
+    }
+}
