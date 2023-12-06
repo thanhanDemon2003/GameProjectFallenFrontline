1b2b511489b65e4c1314a49029dc04ee47dd3b3d
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-12-06 19:10:36 +0700

    Update ApiReward.cs

--- Assets/Resources/UI/Reward/ApiReward.cs
+++ Assets/Resources/UI/Reward/ApiReward.cs
@@ -1,31 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.Networking;
-
+using PlayerModel;
 public class ApiReward : MonoBehaviour
 {
     public static ApiReward rewardApi;
-    public static TMPro.TextMeshProUGUI msg;
-    void Start()
+    public static string msg;
+    public string filePlayer;
+    void Awake()
     {
-    }
-    public void testAPi(string id, string TimeCount, int dotcoin)
-    { 
-        StartCoroutine(UpRewardMode1(id, TimeCount, dotcoin));
-        StartCoroutine(UpRewardMode2(id, TimeCount, dotcoin));
+        filePlayer = Application.persistentDataPath + "/Player.json";
+
     }
 
-
-    IEnumerator UpRewardMode1 (string id, string TimeCount, int dotcoin)
+    IEnumerator UpRewardMode1 (string playingTime, int dotcoin)
     {
-       WWWForm form = new WWWForm();
+        Data data = JsonUtility.FromJson<Data>(File.ReadAllText(filePlayer));
+        var id = data._id;
+        WWWForm form = new WWWForm();
         form.AddField("id_Player", id);
-        form.AddField("playingTime", TimeCount);
+        form.AddField("playingTime", playingTime);
         form.AddField("gameMode", 1);
         form.AddField("dotcoin", dotcoin);
-        Debug.Log("id: " + id + " TimeCount: " + TimeCount + " dotcoin: " + dotcoin);
+        Debug.Log("id: " + id + " TimeCount: " + playingTime + " dotcoin: " + dotcoin);
         UnityWebRequest request = UnityWebRequest.Post("https://darkdisquitegame.andemongame.tech/games/upreward", form);
         yield return request.SendWebRequest();
         if (request.result != UnityWebRequest.Result.Success)
@@ -38,23 +38,25 @@ public class ApiReward : MonoBehaviour
         }
 
     }
-    IEnumerator UpRewardMode2 (string id, string TimeCount, int dotcoin)
+    IEnumerator UpRewardMode2 (string playingTime, int dotcoin)
     {
-       WWWForm form = new WWWForm();
+        Data data = JsonUtility.FromJson<Data>(File.ReadAllText(filePlayer));
+        var id = data._id;
+        WWWForm form = new WWWForm();
         form.AddField("id", id);
-        form.AddField("playingTime", TimeCount);
+        form.AddField("playingTime", playingTime);
         form.AddField("gameMode", 2);
         form.AddField("dotcoin", dotcoin);
         UnityWebRequest request = UnityWebRequest.Post("https://darkdisquitegame.andemongame.tech/games/upreward", form);
         yield return request.SendWebRequest();
         if (request.result != UnityWebRequest.Result.Success)
         {
-            msg.text = "Unable to connect";
+            msg = "Unable to connect";
             Debug.Log(request.error);
         }
         else
         {
-            msg.text = "You have received " + dotcoin + " dotcoin";
+            msg = "You have received " + dotcoin + " dotcoin";
             Debug.Log("Form upload complete!");
         }
 
