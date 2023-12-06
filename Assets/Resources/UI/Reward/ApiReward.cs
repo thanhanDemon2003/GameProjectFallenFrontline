599ee6ec6618f20d525e314d01e310411301292a
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-12-06 18:20:03 +0700

    themCongCoin

---
+++ Assets/Resources/UI/Reward/ApiReward.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiReward : MonoBehaviour
+{
+    public static ApiReward rewardApi;
+    public static TMPro.TextMeshProUGUI msg;
+    void Start()
+    {
+    }
+    public void testAPi(string id, string TimeCount, int dotcoin)
+    { 
+        StartCoroutine(UpRewardMode1(id, TimeCount, dotcoin));
+        StartCoroutine(UpRewardMode2(id, TimeCount, dotcoin));
+    }
+
+
+    IEnumerator UpRewardMode1 (string id, string TimeCount, int dotcoin)
+    {
+       WWWForm form = new WWWForm();
+        form.AddField("id_Player", id);
+        form.AddField("playingTime", TimeCount);
+        form.AddField("gameMode", 1);
+        form.AddField("dotcoin", dotcoin);
+        Debug.Log("id: " + id + " TimeCount: " + TimeCount + " dotcoin: " + dotcoin);
+        UnityWebRequest request = UnityWebRequest.Post("https://darkdisquitegame.andemongame.tech/games/upreward", form);
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+
+    }
+    IEnumerator UpRewardMode2 (string id, string TimeCount, int dotcoin)
+    {
+       WWWForm form = new WWWForm();
+        form.AddField("id", id);
+        form.AddField("playingTime", TimeCount);
+        form.AddField("gameMode", 2);
+        form.AddField("dotcoin", dotcoin);
+        UnityWebRequest request = UnityWebRequest.Post("https://darkdisquitegame.andemongame.tech/games/upreward", form);
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+
+    }
+}
---
+++ Assets/Resources/UI/Reward/Model.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model {
+    public string succsess;
+    public string notification;
+    Model(string succsess, string notification)
+    {
+        this.succsess = succsess;
+        this.notification = notification;
+    }
+}
