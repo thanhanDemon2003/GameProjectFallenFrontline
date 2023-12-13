c847117072fd0b812bdf0e58b176b79f8ab1efca
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-12-13 18:07:31 +0700

    Update HomeScript.cs

--- Assets/Resources/UI/Home/HomeScript.cs
+++ Assets/Resources/UI/Home/HomeScript.cs
@@ -30,6 +30,7 @@ public class HomeScript : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        Time.timeScale =  1f;
         filePathPlayer = Application.persistentDataPath + "/player.json";
         if (!File.Exists(filePathPlayer))
         {
