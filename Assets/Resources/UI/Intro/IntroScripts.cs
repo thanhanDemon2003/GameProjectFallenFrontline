d93af01e528a093f95bdd490de0b8baa4f5bb658
thanhanDemon2003 <iamdemon.dev@gmail.com>
2023-12-16 19:28:52 +0700

    upCreatSkipVideo

--- Assets/Resources/UI/Intro/IntroScripts.cs
+++ Assets/Resources/UI/Intro/IntroScripts.cs
@@ -7,11 +7,33 @@ using UnityEngine.Video;
 public class IntroScripts : MonoBehaviour
 {
     public VideoPlayer video;
+    public VideoPlayer video2;
     void Awake()
     {
-        video.loopPointReached += LoadNextScene;
+        video.Play();
+        video.loopPointReached += CheckVideo1Finished;
+        video2.gameObject.SetActive(false);
+        
     }
 
+    void CheckVideo1Finished(VideoPlayer vp)
+    {
+        video.gameObject.SetActive(false);
+        video2.gameObject.SetActive(true);
+        video2.Play();
+
+        video2.loopPointReached += LoadNextScene;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {   if(video.isPlaying)
+            {
+                return;
+            }
+            LoadNextScene(video2);
+        }
+    }
     void LoadNextScene(VideoPlayer vp)
     {
         SceneManager.LoadSceneAsync(1);
